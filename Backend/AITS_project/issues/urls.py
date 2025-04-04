3a5e46e991bbd00faaafda4ecb1a8c22723678a9
reinol martin <reinolmartin01@gmail.com>
2025-04-04 15:07:56 +0300

    new changes

--- Backend/AITS_project/issues/urls.py
+++ Backend/AITS_project/issues/urls.py
@@ -3,6 +3,8 @@ from .views import IssueListCreateView, IssueDetailView, CommentListCreateView, 
 
 urlpatterns = [
     path('issues/', IssueListCreateView.as_view(), name='issue-list-create'),
-    
+    path('issues/<int:pk>/', IssueDetailView.as_view(), name='issue-detail'),
+    path('issues/<int:issue_id>/comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('notifications/', NotificationListView.as_view(), name='notification-list'),
     path('notifications/<int:pk>/mark-read/', MarkNotificationReadView.as_view(), name='mark-notification-read'),
 ]
