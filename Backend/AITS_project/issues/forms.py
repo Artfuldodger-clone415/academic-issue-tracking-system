bb48be9806b86e392da439e2b3c7f97b8598a1ff
arnoldharun <harunk3570@gmail.com>
2025-04-15 13:37:32 +0300

    Auto-commit on save: forms.py

--- Backend/AITS_project/issues/forms.py
+++ Backend/AITS_project/issues/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User, Issue, Comment
 
-class CustomUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm): 
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'role', 'phone_number', 
