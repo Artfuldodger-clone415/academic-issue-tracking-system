2cf1d7bc3e66fbb677218ff812180733bd07c73f
arnoldharun <harunk3570@gmail.com>
2025-04-15 13:06:56 +0300

    updated meta data

--- Backend/AITS_project/issues/models.py
+++ Backend/AITS_project/issues/models.py
@@ -102,7 +102,7 @@ class Notification(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        ordering = ['-created_at']
-    
+        ordering = ['-created_at'] 
+     
     def __str__(self):
-        return f"{self.notification_type} for {self.user.username}"
+        return f"{self.notification_type} for {self.user.username}"  
