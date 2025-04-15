ab9c195266bf63f62dc4cd059e983a510edbe3e0
arnoldharun <harunk3570@gmail.com>
2025-04-15 13:08:25 +0300

    lecturer permissions

--- Backend/AITS_project/issues/permissions.py
+++ Backend/AITS_project/issues/permissions.py
@@ -17,18 +17,18 @@ class IsStudent(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.role == User.STUDENT 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+class IsOwnerOrReadOnly(permissions.BasePermission): 
+    def has_object_permission(self, request, view, obj): 
+        if request.method in permissions.SAFE_METHODS: 
             return True
-        return obj.created_by == request.user
+        return obj.created_by == request.user 
 
-class IsLecturerAssignedToIssue(permissions.BasePermission):
+class IsLecturerAssignedToIssue(permissions.BasePermission): 
     """
     Custom permission to only allow lecturers assigned to an issue to perform actions.
     """
     
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj): 
         # Check if user is a lecturer and is assigned to this issue
         return (request.user.role == User.LECTURER and 
                 obj.assigned_to == request.user)
