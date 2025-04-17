1e2d227a7643f08749cd6075581c956539faa83f
reinol martin <reinolmartin01@gmail.com>
2025-04-17 18:19:19 +0300

    new changes: modified models.py

--- Backend/AITS_project/issues/models.py
+++ Backend/AITS_project/issues/models.py
@@ -20,7 +20,6 @@ class User(AbstractUser):
         (ADMIN, 'Admin'),
     ]
     
-
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=STUDENT)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     
