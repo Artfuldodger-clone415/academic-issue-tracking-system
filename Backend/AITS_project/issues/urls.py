06dab65946eb7ea87388a8eb288c117b926ba87e
reinol martin <reinolmartin01@gmail.com>
2025-04-17 18:12:02 +0300

    new changes: modified urls.py

--- Backend/AITS_project/issues/urls.py
+++ Backend/AITS_project/issues/urls.py
@@ -51,3 +51,5 @@ urlpatterns = [
     path('students/', UserListView.as_view(), {'role': User.STUDENT}, name='students'),
     path('registrars/', UserListView.as_view(), {'role': User.ACADEMIC_REGISTRAR}, name='registrars'),
 ]
+
+
