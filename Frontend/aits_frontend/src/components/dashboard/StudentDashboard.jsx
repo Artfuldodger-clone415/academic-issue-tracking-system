01657630182a49606c8a66104eff09db19210c1b
reinol martin <reinolmartin01@gmail.com>
2025-04-22 18:11:23 +0300

    new changes: modified StudentDashboard.jsx

--- Frontend/aits_frontend/src/components/dashboard/StudentDashboard.jsx
+++ Frontend/aits_frontend/src/components/dashboard/StudentDashboard.jsx
@@ -13,7 +13,7 @@ const StudentDashboard = ({ stats }) => {
 
 
   useEffect(() => {
-    const fetchRecentIssues = async () => {
+    c
       try {
         const response = await api.get("/issues/")
         // Filter issues created by the current student
