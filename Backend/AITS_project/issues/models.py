d7ef0275db03b698727b89fa03f1e05c85566576
reinol martin <reinolmartin01@gmail.com>
2025-04-22 18:04:22 +0300

    new changes: modified models.py

--- Backend/AITS_project/issues/models.py
+++ Backend/AITS_project/issues/models.py
@@ -30,9 +30,7 @@ class User(AbstractUser):
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}".strip()
 
-class Issue(models.Model):
-    PENDING = 'pending'
-    IN_PROGRESS = 'in_progress'
+c
     RESOLVED = 'resolved'
     CLOSED = 'closed'
     
