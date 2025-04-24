b74d9e81c08beb4af50a9e2aee6cd02ce31e737a
arnoldharun <harunk3570@gmail.com>
2025-04-24 10:20:16 +0300

    modified token_views.py

--- Backend/AITS_project/issues/token_views.py
+++ Backend/AITS_project/issues/token_views.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 import logging
 
 logger = logging.getLogger(__name__)
-
+ 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         try:
