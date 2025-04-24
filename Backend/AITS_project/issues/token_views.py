bad6646281353f837d307d61711132da7663359a
arnoldharun <harunk3570@gmail.com>
2025-04-24 10:20:12 +0300

    modified token_views.py

--- Backend/AITS_project/issues/token_views.py
+++ Backend/AITS_project/issues/token_views.py
@@ -17,7 +17,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
                 'role': user.role,  
                 'first_name': user.first_name,
                 'last_name': user.last_name,
-                'email': user.email,
+                'email': user.email, 
                 'college': user.college 
             }) 
             
