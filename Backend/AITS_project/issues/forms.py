9b39a0f1bfee330c39a539dcea20d7260af214fd
arnoldharun <harunk3570@gmail.com>
2025-04-24 10:18:55 +0300

    modified forms.py

--- Backend/AITS_project/issues/forms.py
+++ Backend/AITS_project/issues/forms.py
@@ -67,7 +67,7 @@ class IssueForm(forms.ModelForm):
 
 class CommentForm(forms.ModelForm): 
     class Meta:
-        model = Comment
+        model = Comment 
         fields = ('content',) 
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3}), 
