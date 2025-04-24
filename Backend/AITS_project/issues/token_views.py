1c190fca892967b65f32b5cdd6f4de60fc3fb634
arnoldharun <harunk3570@gmail.com>
2025-04-24 10:19:27 +0300

    modified token_views.py

--- Backend/AITS_project/issues/token_views.py
+++ Backend/AITS_project/issues/token_views.py
@@ -14,7 +14,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
             data.update({
                 'id': user.id,
                 'username': user.username,
-                'role': user.role, 
+                'role': user.role,  
                 'first_name': user.first_name,
                 'last_name': user.last_name,
                 'email': user.email,
