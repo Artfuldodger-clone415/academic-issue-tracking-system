68d71ceb8c377f00aa0c3d5a90b5965e6e36fabe
OYOKA DANIEL <oyokadaniel415@gmail.com>
2025-04-25 08:21:46 +0300

    modified for proper statisticts display

--- Frontend/aits_frontend/src/components/dashboard/RegistrarDashboard.jsx
+++ Frontend/aits_frontend/src/components/dashboard/RegistrarDashboard.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom" 
 import api from "../../services/api"
-import { useAuth } from "../../contexts/AuthContext"
+import { useAuth } from "../../contexts/AuthContext" 
 import {
   BarChart,
   Bar,
