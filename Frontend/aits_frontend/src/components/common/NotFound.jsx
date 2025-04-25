bb9a1b6bfff99fe511dc95f18973bdd52b027e3c
OYOKA DANIEL <oyokadaniel415@gmail.com>
2025-04-25 08:23:48 +0300

    modified

--- Frontend/aits_frontend/src/components/common/NotFound.jsx
+++ Frontend/aits_frontend/src/components/common/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <div className="not-found-container">
       <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <p>The page you are looking for does not exist.</p> 
       <Link to="/" className="btn btn-primary">
         Return to Dashboard
       </Link>
