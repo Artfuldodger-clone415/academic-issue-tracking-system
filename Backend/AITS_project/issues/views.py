98dd04cd1a3f3a30aea3744f8e96094ca6843bb6
OYOKA DANIEL <oyokadaniel415@gmail.com>
2025-04-25 08:31:28 +0300

    Update views.py

--- Backend/AITS_project/issues/views.py
+++ Backend/AITS_project/issues/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions, status, generics
 from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework.decorators import action 
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
