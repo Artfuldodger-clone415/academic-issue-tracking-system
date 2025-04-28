72bb82933ead28f71b74f620c67ed082003ea018
OYOKA DANIEL <oyokadaniel415@gmail.com>
2025-04-28 13:23:35 +0300

    Update LecturerRegistration.jsx

--- Frontend/aits_frontend/src/components/auth/LecturerRegistration.jsx
+++ Frontend/aits_frontend/src/components/auth/LecturerRegistration.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios'; 
 
+
 const LecturerRegistration = () => { 
   const navigate = useNavigate();
   const [colleges, setColleges] = useState([]);
