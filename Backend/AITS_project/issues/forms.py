93fcc96b7de7d417fe07169c98c0d7b2c6017c7f
OYOKA DANIEL <oyokadaniel415@gmail.com>
2025-04-28 13:09:27 +0300

    Update forms.py

--- Backend/AITS_project/issues/forms.py
+++ Backend/AITS_project/issues/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm 
 from .models import User, Issue, Comment
 
- 
+ # Form creation
 class CustomUserCreationForm(UserCreationForm):
 
 
