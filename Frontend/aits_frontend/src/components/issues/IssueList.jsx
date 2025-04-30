5be5e7f20d5ba8add9d392fc37a5e6e2792f1694
OYOKA DANIEL <oyokadaniel415@gmail.com>
2025-04-30 12:27:13 +0300

    Update IssueList.jsx

--- Frontend/aits_frontend/src/components/issues/IssueList.jsx
+++ Frontend/aits_frontend/src/components/issues/IssueList.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import { useAuth } from "../../contexts/AuthContext"
 import api from "../../services/api"
-import { Search, Filter, Plus, AlertCircle, Clock, Check, X } from 'lucide-react'
+import { Search, Filter, Plus, AlertCircle, Clock, Check, X } from 'lucide-react'  
 
 const IssueList = () => {
   const { user } = useAuth();
