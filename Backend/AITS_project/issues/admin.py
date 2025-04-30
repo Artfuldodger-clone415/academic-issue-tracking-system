e35632ddf594770207bd2beabc31025b97b7d323
OYOKA DANIEL <oyokadaniel415@gmail.com>
2025-04-30 12:22:23 +0300

    Update admin.py

--- Backend/AITS_project/issues/admin.py
+++ Backend/AITS_project/issues/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Issue, Comment, Notification
+from .models import User, Issue, Comment, Notification   
 # registering and creating users roles
 class CustomUserAdmin(UserAdmin): 
     model = User
