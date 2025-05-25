a107ed397fec21fa34d153944a655553674a10fa
OYOKA DANIEL <oyokadaniel415@gmail.com>
2025-05-25 16:19:19 +0300

    Update settings.py

--- Backend/AITS_project/AITS_project/settings.py
+++ Backend/AITS_project/AITS_project/settings.py
@@ -7,11 +7,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "psua4*o1&+-r_5b=!#g*n9p)_oy0k+h4)sima0921u8ss%w(t2")
 
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
-
-
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
@@ -36,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,10 +73,10 @@ DATABASES = {
     }
 }
 
-
+# Custom user model
 AUTH_USER_MODEL = 'issues.User'
 
-
+# REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -86,7 +86,7 @@ REST_FRAMEWORK = {
     ],
 }
 
-
+# JWT settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
@@ -115,7 +115,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+# Add this to your existing settings.py file
 
 LOGGING = {
     'version': 1,
@@ -152,3 +152,6 @@ LOGGING = {
         },
     },
 }
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
